319b2b09a7d79d38bd4b5d98b4c56c0c6279f731
Hungdqhe171841 <hungdqhe171841@fpt.edu.vn>
2026-03-16 02:12:22 +0700

    AddSupplier

--- supermarket/supermarket/src/main/java/com/supermarket/supermarket/controller/SupplierController.java
+++ supermarket/supermarket/src/main/java/com/supermarket/supermarket/controller/SupplierController.java
@@ -1,11 +1,16 @@
 package com.supermarket.supermarket.controller;
 
+import com.supermarket.supermarket.dto.request.CreateSupplierRequest;
 import com.supermarket.supermarket.dto.response.SupplierListItemResponse;
 import com.supermarket.supermarket.service.SupplierService;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +24,13 @@ public class SupplierController {
     @GetMapping
     public ResponseEntity<List<SupplierListItemResponse>> getAllSuppliers() {
         return ResponseEntity.ok(supplierService.getAllSuppliers());
+    }
+
+    @PostMapping
+    public ResponseEntity<SupplierListItemResponse> createSupplier(
+        @Valid @RequestBody CreateSupplierRequest request
+    ) {
+        SupplierListItemResponse created = supplierService.createSupplier(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 }
--- supermarket/supermarket/src/main/java/com/supermarket/supermarket/service/SupplierService.java
+++ supermarket/supermarket/src/main/java/com/supermarket/supermarket/service/SupplierService.java
@@ -1,9 +1,12 @@
 package com.supermarket.supermarket.service;
 
+import com.supermarket.supermarket.dto.request.CreateSupplierRequest;
 import com.supermarket.supermarket.dto.response.SupplierListItemResponse;
 import java.util.List;
 
 public interface SupplierService {
 
     List<SupplierListItemResponse> getAllSuppliers();
+
+    SupplierListItemResponse createSupplier(CreateSupplierRequest request);
 }
